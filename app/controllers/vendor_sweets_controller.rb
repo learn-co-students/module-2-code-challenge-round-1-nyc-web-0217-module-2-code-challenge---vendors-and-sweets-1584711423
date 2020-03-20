1d5ce893893817c22082ab763b55f1be9c46356c
anoorzi <anoorzi123@gmail.com>
2020-03-20 11:22:09 -0400

    final

--- app/controllers/vendor_sweets_controller.rb
+++ app/controllers/vendor_sweets_controller.rb
@@ -8,8 +8,16 @@ class VendorSweetsController < ApplicationController
     end
 
     def create
-        vendor_sweet = VendorSweet.create(vs_params)
-        redirect_to vendor_path(vendor_sweet.vendor_id)
+        @vendor_sweet = VendorSweet.create(vs_params)
+        if @vendor_sweet.valid?
+            flash[:success]= "Successful create!!!"
+            redirect_to vendor_path(vendor_sweet.vendor_id)
+        else
+            flash[:errors]= @vendor_sweet.errors.full_messages
+            redirect_to new_vendor_sweet_path
+
+        end
+        
 
 
     end
