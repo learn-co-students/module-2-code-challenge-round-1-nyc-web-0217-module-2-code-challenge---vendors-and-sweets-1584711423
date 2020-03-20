d43dea56c56577323f33f2126645aa202273d4e4
github username <github email address>
2020-03-20 11:22:05 -0400

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :vendor_sweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vendors
+  resources :vendors, only:[:index, :show]
   resources :sweets
   resources :vendor_sweets
   
