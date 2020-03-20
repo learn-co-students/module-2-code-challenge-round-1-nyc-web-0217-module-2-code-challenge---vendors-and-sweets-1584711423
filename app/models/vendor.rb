669cc5561cdbbf14c98aea887460523a21be7985
github username <github email address>
2020-03-20 10:54:40 -0400

    Phase 3

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,12 @@
 class Vendor < ApplicationRecord
     has_many :vendor_sweets
     has_many :sweets , through: :vendor_sweets
+
+    def avg_price
+       total= self.vendor_sweets.map{|vs|vs.price}.sum
+       amount=self.vendor_sweets.count
+        avg=total/amount
+        avg
+    end
+
 end
--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -3,6 +3,8 @@ class VendorSweet < ApplicationRecord
     belongs_to :sweet
 
     validates :price, numericality: true
-    validates :price, numericality: {greater_than }
+    validates :price, numericality: {greater_than: 0 }
+    # not sure if they are asking this..
+    # validates :vendor_id, :sweet_id,  uniqueness: true
 
 end
