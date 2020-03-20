a7802d38da1eff837634bc177900ee21fc0b1790
github username <github email address>
2020-03-20 11:22:19 -0400

    final commit

--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,5 +1,12 @@
 class Sweet < ApplicationRecord
     has_many :vendor_sweets
     has_many :vendors, through: :vendor_sweets
+    
+    def avg_price
+        total= self.vendor_sweets.map{|vs|vs.price}.sum
+        amount=self.vendor_sweets.count
+         avg=total/amount
+         avg
+     end
 
 end
--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -2,9 +2,9 @@ class VendorSweet < ApplicationRecord
     belongs_to :vendor
     belongs_to :sweet
 
-    validates :price, numericality: true
+    # validates :price, numericality: true
     validates :price, numericality: {greater_than: 0 }
     # not sure if they are asking this..
-    # validates :vendor_id, :sweet_id,  uniqueness: true
+    validates :vendor_id,  uniqueness: {scope: :sweet_id, message:"find another sweet" }
 
 end
