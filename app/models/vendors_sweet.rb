0c9430261b844f9e8b53da31ba2b90c33d309ea6
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-20 10:52:43 -0400

    Everything but 9 is done

--- app/models/vendors_sweet.rb
+++ app/models/vendors_sweet.rb
@@ -1,4 +1,9 @@
 class VendorsSweet < ApplicationRecord
 	belongs_to :sweet 
 	belongs_to :vendor
+
+	validates_uniqueness_of :sweet, scope: :vendor_id
+
+	validates :price, presence: true
+	validates :price, :numericality => { :greater_than_or_equal_to => 0 }
 end
