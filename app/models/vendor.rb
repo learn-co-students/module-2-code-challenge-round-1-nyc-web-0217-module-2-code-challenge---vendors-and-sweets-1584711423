fd2f5ab9a6998b994250827dc35d4f1f0bc96a4f
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-20 11:16:07 -0400

    Added average prices per vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,18 @@
 class Vendor < ApplicationRecord
 	has_many :vendors_sweets 
 	has_many :sweets, through: :vendors_sweets
+
+	def average_price
+		total = 0
+		self.vendors_sweets.each do |vendorssweet|
+			total += vendorssweet.price
+		end
+		if self.vendors_sweets.length != 0
+			average = total / self.vendors_sweets.length
+		else 
+			average = 0
+		end
+		average
+	end
+
 end
