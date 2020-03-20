f3270ee9ea7c84fa8727c76542594b5c8d433d65
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-20 10:34:44 -0400

    finished 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
+  resources :sweets, only: [:index, :show]
   resources :vendors_sweets, only: [:new, :create]
   resources :vendors, only: [:index, :show]
 end
